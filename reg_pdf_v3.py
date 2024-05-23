32cc179178e6145b175ae0166f0233f904e884dc
Jose Miguel Maldonado Garcia <in_secret@hotmail.com>
2024-05-22 19:50:15 -0500

    Se logra obtener el registro pero en la parte inferior

--- reg_pdf_v3.py
+++ reg_pdf_v3.py
@@ -19,12 +19,20 @@ def agregar_texto_a_pdf(ruta_carpeta):
                 pagina = doc[0]
 
                 # Define las propiedades del texto
-                color = (0, 0, 0)  # Color del texto en RGB
+                color = (0, 0, 0)  # Color del texto en RGB, rojo para mayor visibilidad
                 fuente = 'helv'  # Fuente Helvetica
                 tamano = 12  # Tamaño de la fuente
 
+                # Obtiene las dimensiones de la hoja
+                ancho, alto = pagina.rect.width, pagina.rect.height
+
                 # Define la ubicación del texto en la esquina superior derecha
-                ubicacion = fitz.Rect(520, 0, 680, 30)
+                # ajustando las coordenadas en función del tamaño de la hoja
+                x1 = ancho - 150  # 150 es la distancia desde la esquina derecha
+                y1 = alto - 50  # 50 es la distancia desde la esquina superior
+                x2 = ancho - 10  # 10 es el margen derecho
+                y2 = alto - 10  # 10 es el margen superior
+                ubicacion = fitz.Rect(x1, y1, x2, y2)
 
                 # Agrega el texto a la página
                 pagina.insert_textbox(ubicacion, texto, fontname=fuente, fontsize=tamano, color=color)
